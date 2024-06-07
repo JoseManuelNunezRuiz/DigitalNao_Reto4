30e5181f7500dfeb077e70603bf2f6d9ade01193
José Ruiz <143463950+JoseManuelNunezRuiz@users.noreply.github.com>
2024-06-07 08:43:43 -0600

    Create enviar_correo.sh
    Bash para programar el envío de correos automático cada lunes a las 8am

---
+++ enviar_correo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Descargar el script en Python desde GitHub
+curl -O https://raw.githubusercontent.com/JoseManuelNunezRuiz/DigitalNao_Reto4/main/enviar_correo.py
+
+# Ejecutar el script de Python que envía el correo de saludo
+python3 enviar_correo.py
